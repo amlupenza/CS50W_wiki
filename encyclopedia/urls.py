dfd9274d7e30e2f7e537287e295367096579ac7a
amlupenza <amlupenza@gmail.com>
2023-10-04 08:28:42 +0300

    add page feature works, started edit.html

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,6 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:title>", views.entry_page, name="entry_page"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("create/", views.create_page, name="create")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -20,7 +20,9 @@ def entry_page(request, title):
             "title":title, "html":html
         })
     except TypeError:
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html", {
+            "error": "Sorry, entry does not exist!"
+        })
 
 # this function allow user to search for the title of the page and redirected to the page. It gets user's input from the form via post.
 def search(request):
@@ -40,12 +42,33 @@ def search(request):
                 return HttpResponseRedirect(reverse("encyclopedia:entry_page", args=[us_input]))
         # create a new list with it's content in lowercase      
         entries = [entry.lower() for entry in entries]
+        # find the closes match from the user input
         matches = difflib.get_close_matches(us_input.lower(), entries, cutoff=0.02)
         print(f"these are matches:{matches} and this is input: {us_input}")
         return render(request, "encyclopedia/index.html", {
             "entries": matches
         })
-            
+    # if method is get just render an index page      
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
         })
+
+# create page function
+def create_page(request):
+    # check request method is post
+    if request.method == "POST":
+        # retrieve user's input
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        entries = util.list_entries()
+        # turn all markdown file names into lower case
+        entries = [entry.lower() for entry in entries]
+        print(f"this is title:{title}, this is content:{content}")
+        if title.lower() in entries:
+            return render(request, "encyclopedia/error.html", {
+                "error": "Sorry, there is another entry with similar title"
+            })
+        # save entry
+        util.save_entry(title, content)
+        return HttpResponseRedirect(reverse("encyclopedia:entry_page", args=[title]))
+    return render(request, "encyclopedia/create_page.html")
